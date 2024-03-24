d452f27dc4d5282b841bffbf7bc01008e72de281
virendra-koloapp <119936949+virendra-koloapp@users.noreply.github.com>
2024-03-24 22:28:20 +0530

    Added constant value

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount, years float64 = 1000, 10
 	expectedReturns := 5.5
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturns/100), years)
+	futureReadValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println("Investment Amount  : ", investmentAmount)
 	fmt.Println("Future value  : ", futureValue)
+	fmt.Println("Future real value  : ", futureReadValue)
 }
