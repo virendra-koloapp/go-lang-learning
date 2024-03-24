1fbfaad637b5330d41f9a86024444201dc22bb97
virendra-koloapp <119936949+virendra-koloapp@users.noreply.github.com>
2024-03-24 22:15:45 +0530

    Added float64 type explicitly

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturns = 5.5
-	var years = 10
+	var investmentAmount float64 = 1000
+	var expectedReturns float64 = 5.5
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturns/100), float64(years))
+	var futureValue = investmentAmount * math.Pow((1+expectedReturns/100), years)
 
 	fmt.Println("Investment Amount  : ", investmentAmount)
 	fmt.Println("Future value  : ", futureValue)
